1c8b99b66bd9cde50151ef126a3976c4f5b4d00c
mikael-espinola <mikaelespinolaa@gmail.com>
2023-07-29 19:23:15 -0300

    feito renderização de contatos e de chat no aside, array de contatos disponível para prosseguir com o localstorage

--- components/core/modules/contacts.js
+++ components/core/modules/contacts.js
@@ -1,35 +1,100 @@
 import { v4 as uuidv4 } from "https://jspm.dev/uuid";
-const chatCard = document.querySelector(".chat");
+const chatCards = document.querySelector(".chat-cards");
+const contactListSection = document.querySelector(".contact-list");
+const chat = document.querySelector(".chat");
 const textButton = document.querySelector(".text-button");
 const text = document.querySelector(".texting");
+const text2 = document.querySelector(".texting-2");
 
-const names = ["Fulano", "Beltrano", "Sicrano", "Eevee", "Pikachu"];
+export const contactList = [];
+export const activeChats = [];
 
-const contactList = [];
-
-function createContact(name) {
+function createContact(name, status) {
   const newContact = {
     id: uuidv4(),
     name,
+    status,
     selected: false,
   };
 
   contactList.push(newContact);
-
+  contactList.forEach((contact) => {
+    renderContactList(contact);
+  });
   return newContact;
 }
 
-names.forEach((name) => {
-  createContact(name);
+contactList.forEach((contact) => {
+  renderContactList(contact);
 });
+
+export function renderChatCard(contact) {
+  const newChatCard = document.createElement("div");
+  newChatCard.classList.add("chat");
+  newChatCard.setAttribute("id", contact.id);
+
+  const icon = document.createElement("span");
+  icon.classList.add("material-symbols-outlined");
+  icon.classList.add("face");
+  icon.classList.add("border-chat");
+  icon.textContent = "face";
+  newChatCard.appendChild(icon);
+
+  const contactName = document.createElement("span");
+  contactName.classList.add("contact-name");
+  contactName.textContent = contact.name;
+  newChatCard.appendChild(contactName);
+
+  const message = document.createElement("span");
+  message.classList.add("message");
+  message.classList.add("border-chat");
+  message.textContent = "Hoje tem! kwjdnw ncwdnf dcmckdm cvkdmopsdmds";
+  newChatCard.appendChild(message);
+
+  const time = document.createElement("span");
+  time.classList.add("time");
+  time.textContent = new Date();
+  newChatCard.appendChild(time);
+
+  chatCards.appendChild(newChatCard);
+}
+
+function renderContactList(contact) {
+  const contactToList = document.createElement("div");
+  contactToList.classList.add("chat");
+  contactToList.classList.add("contact");
+  contactToList.setAttribute("id", contact.id);
+
+  const icon = document.createElement("span");
+  icon.classList.add("material-symbols-outlined");
+  icon.classList.add("face");
+  icon.classList.add("border-chat");
+  icon.textContent = "face";
+  contactToList.appendChild(icon);
+
+  const contactName = document.createElement("span");
+  contactName.classList.add("contact-name");
+  contactName.textContent = contact.name;
+  contactToList.appendChild(contactName);
+
+  const message = document.createElement("span");
+  message.classList.add("message");
+  message.classList.add("border-chat");
+  message.textContent = contact.message;
+  contactToList.appendChild(message);
+
+  contactListSection.appendChild(contactToList);
+}
 
 textButton.addEventListener("click", (e) => {
   e.preventDefault();
   const name = text.value;
+  const status = text2.value;
 
-  createContact(name);
+  createContact(name, status);
   console.log(contactList);
 
   text.value = "";
   text.focus();
+  text2.value = "";
 });
